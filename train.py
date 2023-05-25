a7d1197a460dd98c524517aa1eede33b19b1e7ad
Ken Wakita <wakita@is.titech.ac.jp>
2023-05-25 17:07:01 +0900

    rewrite

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import atexit
 import argparse
 from datetime import datetime
 import yaml
@@ -13,8 +12,6 @@ from utils.trainer import Trainer
 from utils.helper import get_model_class, get_optimizer_class, get_lr_scheduler, save_config, save_vocab
 
 def train(config):
-  os.makedirs(config['run_dir'], exist_ok=True)
-
   train_dataloader, vocab = get_dataloader_and_vocab(ds_type='train', batch_size=config['train']['batch_size'],
                                                      **config)
   valid_dataloader, _     = get_dataloader_and_vocab(ds_type='valid', batch_size=config['valid']['batch_size'],
@@ -33,18 +30,17 @@ def train(config):
   device = torch.device('cpu')
 
   C = dict(config,
-           **dict(model=model,
-                  train_dataloader=train_dataloader, train_steps=config['train']['steps'],
-                  valid_dataloader=valid_dataloader, valid_steps=config['valid']['steps'],
-                  criterion=criterion, optimizer=optimizer, lr_scheduler=lr_scheduler,
-                  device=device))
-  trainer = Trainer(C)
+           model=model,
+           train_dataloader=train_dataloader, train_steps=config['train']['steps'],
+           valid_dataloader=valid_dataloader, valid_steps=config['valid']['steps'],
+           criterion=criterion, optimizer=optimizer, lr_scheduler=lr_scheduler,
+           device=device)
 
+  trainer = Trainer(C)
   trainer.train()
   print('\nTraining finished.')
 
   trainer.save_model()
-  trainer.save_loss()
   save_vocab(vocab, C)
   save_config(config)
   print('\nModel artifacts saved to folder:', C['run_dir'])
@@ -59,9 +55,9 @@ if __name__ == '__main__':
     C = yaml.safe_load(f)
     now = datetime.now().strftime('%Y-%m-%d_%H-%M')
     C['run_dir'] = os.path.join('var', f'{C["model_name"]}_{C["dataset"]}', now)
+    os.makedirs(C['run_dir'], exist_ok=True)
 
     # Tensorboard サーバを起動。URL が表示されるので、それをブラウザで開けば学習の様子が見られる。
     p = Popen(['tensorboard', '--logdir', C['run_dir']])
-    atexit.register(lambda: p.terminate())
 
     train(C)
--- utils/trainer.py
+++ utils/trainer.py
@@ -1,15 +1,13 @@
 import os
 import numpy as np
-import json
 import torch
 from torchinfo import summary
 from tensorboardX import SummaryWriter
 
 class Trainer:
   def __init__(self, config):
-    C = self.C = config
-    self.loss = {'train': [], 'valid': []}
-    self.writer = SummaryWriter(C['run_dir'])
+    self.C = config
+    self.writer = SummaryWriter(config['run_dir'])
 
   def train(self):
     C = self.C
@@ -17,8 +15,6 @@ class Trainer:
       self.writer.add_scalar('learning_rate', C['lr_scheduler'].get_last_lr(), epoch)
       self._train_epoch(epoch)
       self._validate_epoch(epoch)
-      train_loss, valid_loss = self.loss['train'][-1], self.loss['valid'][-1]
-      print(f'Epoch: {epoch + 1}/{C["epochs"]}, Train Loss={train_loss:.5f}, Valid Loss={valid_loss:.5f}')
 
       C['lr_scheduler'].step()
       if C['checkpoint_frequency']: self._save_checkpoint(epoch)
@@ -34,7 +30,7 @@ class Trainer:
     running_loss = []
 
     for i, (inputs, labels) in enumerate(train_dataloader, 1):
-      if epoch == 0 and i == 0:
+      if epoch == 0 and i == 1:
         summary(model, input_data=inputs, col_names=['input_size', 'output_size', 'num_params'])
         print()
 
@@ -43,13 +39,12 @@ class Trainer:
       optimizer.zero_grad()
       loss = criterion(model(inputs), labels)
       loss.backward()
+      optimizer.step()  # モデルを更新する
       running_loss.append(loss.item())
-      optimizer.step()
 
       if i == steps: break
 
-    self.loss['train'].append(np.mean(running_loss))
-    self.writer.add_scalar('loss/train', self.loss['train'][-1], epoch)
+    self.writer.add_scalar('loss/train', np.mean(running_loss), epoch)
 
   def _validate_epoch(self, epoch):
     C = self.C
@@ -69,8 +64,7 @@ class Trainer:
 
         if i == steps: break
 
-    self.loss['valid'].append(np.mean(running_loss))
-    self.writer.add_scalar('loss/valid', self.loss['valid'][-1], epoch)
+    self.writer.add_scalar('loss/valid', np.mean(running_loss), epoch)
 
   def _save_checkpoint(self, epoch):
     epoch_num = epoch + 1
@@ -81,7 +75,3 @@ class Trainer:
   def save_model(self):
     model_path = os.path.join(self.C['run_dir'], 'model.pt')
     torch.save(self.C['model'], model_path)
-
-  def save_loss(self):
-    loss_path = os.path.join(self.C['run_dir'], 'loss.json')
-    with open(loss_path, 'w') as w: json.dump(self.loss, w)
